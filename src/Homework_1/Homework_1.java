a48e3f9bb2d79e5d61adf0c8445f0404df9b56e5
eboyd-24 <158003420+eboyd-24@users.noreply.github.com>
2024-01-29 22:59:43 -0600

    Changed name

--- src/Homework_1/Homework_1.java
+++ src/Homework_1/Homework_1.java
@@ -5,7 +5,7 @@ public class Homework_1 {
 	public static void main(String[] args) {
 		
 		System.out.printf("Name: %s, GitHub ID #%d\n", "Jackson Morgan", 93284001 );
-		System.out.printf("Name: %s, GitHub ID #%d\n", "Ethan Boyd",     158003420);
+		System.out.printf("Name: %s, GitHub ID #%d\n", "Evan Boyd",     158003420);
 		System.out.printf("Name: %s, GitHub ID #%d\n", "Joey Gallichio", 158005169);
 		System.out.printf("Name: %s, GitHub ID #%d\n", "Pablo Lasarte",  158004110);
 
